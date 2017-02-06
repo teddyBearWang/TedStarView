1c8871c9f5cdcf76c57aa4eb0b39a1ed7c2b8539
汪超 <815819325@qq.com>
2017-02-06 10:05:07 +0800

    Initial Commit

---
+++ TeddyStarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TeddyStarDemo
+//
+//  Created by apple on 17/2/6.
+//  Copyright (c) 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TeddyStarDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TeddyStarDemo
+//
+//  Created by apple on 17/2/6.
+//  Copyright (c) 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
